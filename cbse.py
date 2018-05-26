1a2e8b6b451c06d1ae2d4a34472980c8f7c31889
Rushat Gabhane <rushatgabhane@gmail.com>
2018-05-26 15:38:50 +0300

    Update cbse.py

--- cbse.py
+++ cbse.py
@@ -8,14 +8,8 @@ school_number = '72511'
 center_number = '8801'
 roll_number = 9100001  #to be iterated
 
-f = open('marks.txt', 'r')
-x = f.read().splitlines()
-
 roll_box = driver.find_element_by_name("regno").send_keys(roll_number)
-
 school_box = driver.find_element_by_name("sch").send_keys(school_number)
-
 center_box = driver.find_element_by_name("cno").send_keys(center_number)
-
 sumbmit_button = driver.find_element_by_name("B2")
 sumbmit_button.click()
