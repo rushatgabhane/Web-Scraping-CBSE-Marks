4800d3f809ae32d0d34a723f43b98cd379fd8cf5
Rushat Gabhane <rushatgabhane@gmail.com>
2018-05-26 15:10:05 +0300

    elements id added only

---
+++ cbse.py
@@ -0,0 +1,19 @@
+from selenium import webdriver
+import time
+
+driver = webdriver.Chrome()
+driver.get('http://cbseresults.nic.in/class12zpq/class12th18.htm')
+
+filepath = 'marks.txt'
+school_number = '72511'
+center_number = '8801'
+
+f = open('marks.txt', 'r')
+x = f.read().splitlines()
+
+roll_box = driver.find_element_by_name("regno")
+user.click()
+
+school_box = driver.find_element_by_name("sch")
+
+center_box = driver.find_element_by_name("cno")
